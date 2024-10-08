4d0950d421449bb4cc32d92b3f238191de7a21fc
Akiva Levi <akiva147@gmail.com>
2024-10-08 17:46:28 +0300

    chore: deleted unused code, added signup error message

--- src/pages/SignupPage/SignupPage.tsx
+++ src/pages/SignupPage/SignupPage.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router';
 import { Form } from '../../components/Form';
 import { User } from '../../models/user.model';
 import { authService } from '../../services/auth.service';
+import { message } from 'antd';
 
 export interface SignupPageProps {}
 
@@ -13,6 +14,9 @@ export const SignupPage = (props: SignupPageProps) => {
       await authService.signup(data.email, data.password);
       navigate('/login');
     } catch (error) {
+      message.error({
+        content: 'Singup failed. Please check if the user already exist.',
+      });
       console.error('Signup failed', error);
     }
   };
